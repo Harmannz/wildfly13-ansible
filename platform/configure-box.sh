888103b33fd55e276ee8a58f455b8ea2b6c91d3a
Harman Singh <harman.singh@hotmail.co.nz>
2018-07-02 00:26:53 +1200

    Deploy wildfly to separate address

--- platform/configure-box.sh
+++ platform/configure-box.sh
@@ -10,5 +10,5 @@ yum install -y ansible
 echo "Configuring network"
 sudo nmcli con del enp0s8 | true
 sudo nmcli con add type ethernet ifname enp0s8 con-name enp0s8
-sudo nmcli con mod enp0s8 ipv4.addresses '10.128.251.2,10.128.251.3' ipv4.method manual ipv4.routes 10.0.0.0/16
+sudo nmcli con mod enp0s8 ipv4.addresses '10.128.251.2,10.128.251.3,10.128.251.4' ipv4.method manual ipv4.routes 10.0.0.0/16
 sudo systemctl restart network
