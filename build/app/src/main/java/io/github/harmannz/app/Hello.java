f2c66c1c0cdedc5848317d6743624c6e92f05f6d
Harman Singh <harman.singh@hotmail.co.nz>
2018-07-11 19:26:08 +1200

    Setup application logging

--- build/app/src/main/java/io/github/harmannz/app/Hello.java
+++ build/app/src/main/java/io/github/harmannz/app/Hello.java
@@ -2,13 +2,28 @@ package io.github.harmannz.app;
 
 
 import javax.faces.bean.ManagedBean;
+import javax.inject.Inject;
+import org.slf4j.Logger;
 
 @ManagedBean
 public class Hello {
 
+    @Inject
+    private Logger logger;
+
     private static final String world = "Hello World!";
+    private String response;
 
     public String getWorld() {
         return world;
+    }
+
+    public String getResponse(){
+        return response;
+    }
+
+    public void submit(){
+        logger.debug("Hi called!");
+        response = "Hi responded";
     }
 }
